bcf558e371cd2eff607adf6caa7cc3bf8c175ab3
ellimac-backup <geoffroycamille3@gmail.com>
2025-05-18 17:53:45 +0200

    ackerman_robot: clean unused files

--- ackerman_robot/hardware/diffbot_system.cpp
+++ ackerman_robot/hardware/diffbot_system.cpp
@@ -155,50 +155,12 @@ hardware_interface::CallbackReturn DiffDriveArduinoHardware::on_init(
       }
     }    
   }
- /* Bicyle
-  // code
-  hw_start_sec_ = std::stod(info_.hardware_parameters["example_param_hw_start_duration_sec"]);
-  hw_stop_sec_ = std::stod(info_.hardware_parameters["example_param_hw_stop_duration_sec"]);
-  // // END: This part here is for exemplary purposes - Please do not copy to your production code
-
-  hw_interfaces_["steering"] = Joint("left_wheel_steering_joint");
-
-  hw_interfaces_["traction"] = Joint("rear_left_wheel_joint");
-
- */
  
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 std::vector<hardware_interface::StateInterface> DiffDriveArduinoHardware::export_state_interfaces()
 {
-  
-  /*std::vector<hardware_interface::StateInterface> state_interfaces;
-
-  for (auto & joint : hw_interfaces_)
-  {
-    state_interfaces.emplace_back(hardware_interface::StateInterface(
-      joint.second.joint_name, hardware_interface::HW_IF_POSITION, &joint.second.state.position));
-
-      RCLCPP_INFO(get_logger(), "JOINT %s", joint.first.c_str());
-
-    if (joint.first == "traction")
-    {
-      state_interfaces.emplace_back(hardware_interface::StateInterface(
-        joint.second.joint_name, hardware_interface::HW_IF_VELOCITY, &joint.second.state.velocity));
-    }
-  }
-
-  RCLCPP_INFO(get_logger(), "Exported %zu state interfaces.", state_interfaces.size());
-
-  for (auto s : state_interfaces)
-  {
-    RCLCPP_INFO(get_logger(), "Exported state interface '%s'.", s.get_name().c_str());
-  }
-
-  return state_interfaces;*/
-
-  
   std::vector<hardware_interface::StateInterface> state_interfaces;
 
   state_interfaces.emplace_back(hardware_interface::StateInterface(
@@ -224,35 +186,6 @@ std::vector<hardware_interface::StateInterface> DiffDriveArduinoHardware::export
 
 std::vector<hardware_interface::CommandInterface> DiffDriveArduinoHardware::export_command_interfaces()
 {
-  /*std::vector<hardware_interface::CommandInterface> command_interfaces;
-
-  for (auto & joint : hw_interfaces_)
-  {
-    if (joint.first == "steering")
-    {
-      RCLCPP_INFO(get_logger(), "STEERING COMMAND  %s  ", joint.second.joint_name.c_str() );
-      command_interfaces.emplace_back(hardware_interface::CommandInterface(
-        joint.second.joint_name, hardware_interface::HW_IF_POSITION,
-        &joint.second.command.position));
-    }
-    else if (joint.first == "traction")
-    {
-      command_interfaces.emplace_back(hardware_interface::CommandInterface(
-        joint.second.joint_name, hardware_interface::HW_IF_VELOCITY,
-        &joint.second.command.velocity));
-    }
-  }
-
-  RCLCPP_INFO(get_logger(), "Exported %zu command interfaces.", command_interfaces.size());
-
-  for (auto i = 0u; i < command_interfaces.size(); i++)
-  {
-    RCLCPP_INFO(
-      get_logger(), "Exported command interface '%s'.", command_interfaces[i].get_name().c_str());
-  }
-
-  return command_interfaces;*/
-
   std::vector<hardware_interface::CommandInterface> command_interfaces;
 
   command_interfaces.emplace_back(hardware_interface::CommandInterface(
@@ -314,34 +247,6 @@ hardware_interface::CallbackReturn DiffDriveArduinoHardware::on_activate(
   RCLCPP_INFO(rclcpp::get_logger("DiffDriveArduinoHardware"), "Successfully activated!");
 
   return hardware_interface::CallbackReturn::SUCCESS;
-  /*
-  RCLCPP_INFO(get_logger(), "Activating ...please wait...");
-
-  for (auto i = 0; i < hw_start_sec_; i++)
-  {
-    rclcpp::sleep_for(std::chrono::seconds(1));
-    RCLCPP_INFO(get_logger(), "%.1f seconds left...", hw_start_sec_ - i);
-  }
-
-  for (auto & joint : hw_interfaces_)
-  {
-    joint.second.state.position = 0.0;
-
-    if (joint.first == "traction")
-    {
-      joint.second.state.velocity = 0.0;
-      joint.second.command.velocity = 0.0;
-    }
-
-    else if (joint.first == "steering")
-    {
-      joint.second.command.position = 0.0;
-    }
-  }
-
-  RCLCPP_INFO(get_logger(), "Successfully activated!");
-
-  return hardware_interface::CallbackReturn::SUCCESS;*/
 }
 
 hardware_interface::CallbackReturn DiffDriveArduinoHardware::on_deactivate(
--- ackerman_robot/launch/ackermann_hardware.launch.py
+++ ackerman_robot/launch/ackermann_hardware.launch.py
@@ -1,115 +1,115 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessStart
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 
 from launch_ros.actions import Node
-
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
-
-
-
+# controller = "bicycle_steering_controller"
+controller = "ackermann_steering_controller"
 
 def generate_launch_description():
-
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='ackerman_robot'
-
-    rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+    package_name = "ackerman_robot"
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(package_name), "urdf", "ackermann.urdf.xacro"]
+            ),
+        ]
     )
-
-    
-
-
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','ackermann_drive_controller.yaml')
-
-    controller_manager = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description},
-                    controller_params_file]
-    )
-
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    robot_description = {"robot_description": robot_description_content}
 
     robot_controllers = PathJoinSubstitution(
-            [
-                FindPackageShare('ackerman_robot'),
-                'config',
-                'ackermann_drive_controller.yaml',
-            ]
+        [
+            FindPackageShare(package_name),
+            "config",
+            "ackermann_controller.yaml",
+        ]
     )
-    ackermann_steering_controller_spawner = Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['ackermann_steering_controller',
-                       '--param-file',
-                       robot_controllers,
-                       '--controller-ros-args',
-                       '-r /ackermann_steering_controller/tf_odomerty:=/tf',
-                       ],
-            )
-
-    delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[ackermann_steering_controller_spawner],
-        )
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(package_name), "rviz", "carlikebot.rviz"]
     )
 
-    joint_broad_spawner = Node(
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_controllers],
+        output="both",
+    )
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+        remappings=[
+            ("/"+controller+"/reference", "/cmd_vel"),
+        ],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_broad"],
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
     )
 
-    delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[joint_broad_spawner],
+   # robot_controller_spawner = Node(
+    #    package="controller_manager",
+    #    executable="spawner",
+    #    arguments=["ackermann_steering_controller", "--controller-manager", "/controller_manager"],
+    #)
+    robot_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[controller,
+                   '--param-file',
+                   robot_controllers,
+                   '--controller-ros-args',
+                   '-r /'+controller+'/tf_odometry:=/tf',
+                   ],
+    )
+
+    teleop_keyboard = Node(
+            package="teleop_twist_keyboard",
+            executable="teleop_twist_keyboard",
+            prefix="xterm -e",
+            parameters=[{'stamped': True}],
+            remappings=[('cmd_vel','/'+controller+'/reference')]
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
         )
     )
 
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
 
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+    nodes = [
+        control_node,
+        teleop_keyboard,
+        robot_state_pub_node,
+        joint_state_broadcaster_spawner,
+        delay_rviz_after_joint_state_broadcaster_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+    ]
 
-
-
-    # Launch them all!
-    return LaunchDescription([
-        rsp,
-        delayed_controller_manager,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
-    ])
+    return LaunchDescription(nodes)
